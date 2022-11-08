22d4ca2e2fe29f0b63902fbf9737902f9de6e5cd
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-11-08 20:29:10 +0100

    ex1 done

--- src/films.js
+++ src/films.js
@@ -1,23 +1,28 @@
+getAllDirectors(movies);
 // Exercise 1: Get the array of all directors.
-function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
-  return result;
+function getAllDirectors(movies) {
+    let result;
+    // pass a function to map
+    result = movies.map(function(element) {
+        return element.director
+    });
+    console.log("EXERCICE 1 ->", result);
+    return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+
 }
 
 // Exercise 5: Order by year, ascending
@@ -37,7 +42,7 @@ function hoursToMinutes() {
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {
-  
+
 }
 
 
@@ -45,14 +50,14 @@ function bestFilmOfYear() {
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
-  module.exports = {
-    getAllDirectors,
-    getMoviesFromDirector,
-    moviesAverageOfDirector,
-    orderAlphabetically,
-    orderByYear,
-    moviesAverageByCategory,
-    hoursToMinutes,
-    bestFilmOfYear,
-  };
+    module.exports = {
+        getAllDirectors,
+        getMoviesFromDirector,
+        moviesAverageOfDirector,
+        orderAlphabetically,
+        orderByYear,
+        moviesAverageByCategory,
+        hoursToMinutes,
+        bestFilmOfYear,
+    };
 }
